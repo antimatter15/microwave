0ec59281f280cee60c18cc5b229b557627e8f30a
antimatter15 <antimatter15@gmail.com>
2010-07-25 13:07:13 -0400

    moved stuff from opt to globals

--- js/globals.js
+++ js/globals.js
@@ -16,3 +16,9 @@ var current_page = 0; //0 = search, 1 = wave
 var search_outdated = false;
 var searchscroll = 0;
 var scrollto_position = -1;
+
+if(mobilewebkit) document.body.className += ' mobilewebkit'; //yeah i know browser detection is bad, but how do i get around it here? 
+
+if(!window.onLine) window.onLine = function(){return true};
+
+
--- js/opt.js
+++ js/opt.js
@@ -1,9 +1,7 @@
-if(mobilewebkit){
-  document.body.className += ' mobilewebkit'; //yeah i know browser detection is bad, but how do i get around it here? 
-}
-if(!window.onLine){
-	window.onLine = function(){return true};
-}
+opt.x.multipane = 'Enable multipane viewing experience (note, you must reload the page for changes to take effect)'
+opt.x.touchscroll = "Add the TouchScroll library to do cool scrolly things on iPad Multipane"
+
+
 
 
 
@@ -13,7 +11,6 @@ if(opt.multipane === undefined && screen_size > 900 && !mobilewebkit){
 }
 
 
-opt.x.multipane = 'Enable multipane viewing experience (note, you must reload the page for changes to take effect)'
 
 
 if(opt.multipane) {
@@ -76,7 +73,7 @@ function addTouchScroll(){
     document.body.appendChild(script)
 } 
 
-opt.x.touchscroll = "Add the TouchScroll library to do cool scrolly things on iPad Multipane"
+
 if(opt.touchscroll){
   addTouchScroll('wave_container_parent', 'search_parent_container')
   document.getElementById('wave_container_parent').style.width = (innerWidth-300)+'px';
