4bb3413bba5e5e70f65179eff934e1a084d2ebd2
antimatter15 <antimatter15@antimatter15-desktop.(none)>
2010-07-25 09:11:14 -0400

    loading bar does display:none after a timeout, probably buggy

--- microwave.all.js
+++ microwave.all.js
@@ -835,7 +835,6 @@ function loading(text, nodelay){
   //we need to adjust for the possibility that the load is cancelled before it's actually loaded
 	var load = document.getElementById("loading");
 	var has_opacity = typeof document.createElement('div').style.opacity != 'undefined';
-	
 	load.style.top = scrollY+'px';
 	if(typeof text == "number"){
     if(has_opacity)
@@ -843,18 +842,22 @@ function loading(text, nodelay){
 		else
 			load.style.display = "none";
 		delete loadIds[text];
+    setTimeout(function(){
+      load.style.display = 'none';
+    },500)
   }else{
     var id = Math.random()*42;
     loadIds[id] = true;
 		setTimeout(function(){
 		  load.style.top = scrollY+'px';
       if(loadIds[id]){
+				load.style.display = "";
         if(has_opacity)
 					load.style.opacity = "1";
-				else
-					load.style.display = "";
+
         document.getElementById("loadingtext").innerHTML = "<b>Loading</b> "+text;
       }
+
     }, nodelay?0:0); //it's unnerving when things flash, so only show after a wait
     return id;
   }
