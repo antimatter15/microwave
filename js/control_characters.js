124c45eafa1b53b8f006b29a639225adc35e0161
antimatter15 <antimatter15@gmail.com>
2010-07-28 10:28:44 -0400

    removed control-characters.js

--- js/control_characters.js
+++
@@ -1,6 +0,0 @@
-\0-\x09\x0b-\x1f\x7f\x80-\x9f
-\u2028\u2029
-\ufff9\ufffa\ufffb
-\u200e\u200f\u202a-\u202e
-
-replace(/[\0-\x09\x0b-\x1f\x7f\x80-\x9f\u2028\u2029\ufff9\ufffa\ufffb\u200e\u200f\u202a-\u202e]/g,'')
